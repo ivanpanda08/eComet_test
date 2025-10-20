481a5a7c1fe2dbcfa704072774fdcbca8dbd1d17
ivanpanda08 <ivanpanda08@yandex.ru>
2025-10-20 16:40:49 +0300

    2. feat(scraper): добавление ограничений MCR и RPS, реализация параллельной асинхронной обработки репозиториев

--- 2/main.py
+++ 2/main.py
@@ -1,55 +1,22 @@
-from dataclasses import dataclass
-from typing import Final, Any
-
-from aiohttp import ClientSession
-
-GITHUB_API_BASE_URL: Final[str] = "https://api.github.com"
+import asyncio
+import logging
+from scraper import GithubReposScrapper
+from config import get_config
 
 
-@dataclass
-class RepositoryAuthorCommitsNum:
-    author: str
-    commits_num: int
+async def main():
+    config = await get_config()
+    logging.basicConfig(level=config.log_level)
+    scrapper = GithubReposScrapper(
+        access_token=config.github_api_token,
+        max_concurrent_requests=config.max_concurrent_requests,
+        requests_per_second=config.requests_per_second
+    )
+    try:
+        repositories = await scrapper.get_repositories()
+    finally:
+        await scrapper.close()
 
 
-@dataclass
-class Repository:
-    name: str
-    owner: str
-    position: int
-    stars: int
-    watchers: int
-    forks: int
-    language: str
-    authors_commits_num_today: list[RepositoryAuthorCommitsNum]
-
-
-class GithubReposScrapper:
-    def __init__(self, access_token: str):
-        self._session = ClientSession(
-            headers={
-                "Accept": "application/vnd.github.v3+json",
-                "Authorization": f"Bearer {access_token}",
-            }
-        )
-
-    async def _make_request(self, endpoint: str, method: str = "GET", params: dict[str, Any] | None = None) -> Any:
-        async with self._session.request(method, f"{GITHUB_API_BASE_URL}/{endpoint}", params=params) as response:
-            return await response.json()
-
-    async def _get_top_repositories(self, limit: int = 100) -> list[dict[str, Any]]:
-        """GitHub REST API: https://docs.github.com/en/rest/search/search?apiVersion=2022-11-28#search-repositories"""
-        data = await self._make_request(
-            endpoint="search/repositories",
-            params={"q": "stars:>1", "sort": "stars", "order": "desc", "per_page": limit},
-        )
-        return data["items"]
-
-    async def _get_repository_commits(self, owner: str, repo: str) -> list[dict[str, Any]]:
-        """GitHub REST API: https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28#list-commits"""
-        ...
-
-    async def get_repositories(self) -> list[Repository]: ...
-
-    async def close(self):
-        await self._session.close()
+if __name__ == "__main__":
+    asyncio.run(main())
